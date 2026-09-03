262129b690e1d7595ff2477e7f9c0cab95858682
Ramaprasadnayak <ramprasadnayak999@gmail.com>
2026-09-03 16:15:56 +0530

    added district boundary

--- routers/district_boundary.py
+++ routers/district_boundary.py
@@ -1,19 +1,36 @@
 from fastapi import HTTPException, APIRouter
 from config.db import districts_collection
 from rapidfuzz import process, fuzz
-from utils.districts import all_districts
 
 router = APIRouter(
     prefix="/api",
     tags=["boundary"]
 )
 
-
 @router.get("/districts/{district_name}/coordinates")
 async def get_district_coordinates(district_name: str):
+    district_list = list(
+        districts_collection.find(
+            {},
+            {
+                "_id": 0,
+                "district": 1,
+                "district_code": 1
+            }
+        )
+    )
+    if not district_list:
+        raise HTTPException(
+            status_code=404,
+            detail="No districts found"
+        )
+    district_names = [
+        item["district"]
+        for item in district_list
+    ]
     matches = process.extract(
         district_name,
-        [district["district"] for district in all_districts],
+        district_names,
         scorer=fuzz.ratio,
         limit=1
     )
@@ -23,13 +40,12 @@ async def get_district_coordinates(district_name: str):
             detail="District not found"
         )
     matched_name, score, index = matches[0]
-    # Reject bad fuzzy matches
-    if score < 70:
+    if score < 55:
         raise HTTPException(
             status_code=404,
             detail="District not found"
         )
-    matched_district = all_districts[index]
+    matched_district = district_list[index]
     district_code = matched_district["district_code"]
     district = districts_collection.find_one(
         {
@@ -40,7 +56,8 @@ async def get_district_coordinates(district_name: str):
             "district": 1,
             "state": 1,
             "district_code": 1,
-            "geometry": 1
+            "geometry": 1,
+            "center":1
         }
     )
     if not district:
@@ -50,8 +67,20 @@ async def get_district_coordinates(district_name: str):
         )
     geometry = district["geometry"]
     polygons = []
-    for polygon in geometry["coordinates"]:
-        for ring in polygon:
+    if geometry["type"] == "MultiPolygon":
+        for polygon in geometry["coordinates"]:
+            for ring in polygon:
+                converted_ring = []
+                for coordinate in ring:
+                    longitude = coordinate[0]
+                    latitude = coordinate[1]
+                    converted_ring.append({
+                        "lat": latitude,
+                        "lng": longitude
+                    })
+                polygons.append(converted_ring)
+    elif geometry["type"] == "Polygon":
+        for ring in geometry["coordinates"]:
             converted_ring = []
             for coordinate in ring:
                 longitude = coordinate[0]
@@ -61,11 +90,18 @@ async def get_district_coordinates(district_name: str):
                     "lng": longitude
                 })
             polygons.append(converted_ring)
+    else:
+        raise HTTPException(
+            status_code=500,
+            detail=f"Unsupported geometry type: {geometry['type']}"
+        )
     return {
         "message": "retrieved boundary successfully",
         "district": district["district"],
+        "matched_name":matched_name,
         "state": district["state"],
         "district_code": district["district_code"],
         "geometry_type": geometry["type"],
-        "polygons": polygons
+        "polygons": polygons,
+        "center": district["center"]
     }
