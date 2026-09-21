e38a5bee893b8d8a8d1d8081a89e9cf338349fb2
Ramaprasadnayak <ramprasadnayak999@gmail.com>
2026-09-21 21:02:47 +0530

    added docker file

--- config/firebase.py
+++ config/firebase.py
@@ -1,8 +1,16 @@
-import firebase_admin
-from firebase_admin import credentials
+import os
 from pathlib import Path
 
+import firebase_admin
+from firebase_admin import credentials
+
 BASE_DIR = Path(__file__).resolve().parent.parent
-service_account_path = BASE_DIR / "serviceAccountKey.json"
-cred = credentials.Certificate(str(service_account_path))
+
+service_account_path = os.getenv(
+    "FIREBASE_CREDENTIALS",
+    str(BASE_DIR / "serviceAccountKey.json")
+)
+
+cred = credentials.Certificate(service_account_path)
+
 firebase_admin.initialize_app(cred)
